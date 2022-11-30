e5568ef4f8374658d4e33d3adb6d08e555a67681
Abdulhaqq Sule <writeme@abdulhaqqsule.com>
2022-11-30 13:08:12 +0100

    Implemented example variable for DATABASE_NAME

--- src/config/config.js
+++ src/config/config.js
@@ -8,8 +8,8 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    DATABASE_URL: Joi.string().required().description('DATABASE URL'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
+    DATABASE_URL: Joi.string().required().description('URL of MongoDB'),
+    JWT_SECRET: Joi.string().required().description('JWT Secret Key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
