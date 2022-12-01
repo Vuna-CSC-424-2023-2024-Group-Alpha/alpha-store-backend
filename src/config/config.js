7d412394506cd0a16ac01c25a773498a23f112c6
mikey247 <michaeljunior794@gmail.com>
2022-12-01 17:06:03 +0100

    Fixed mongo connection

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,7 @@ const envVarsSchema = Joi.object()
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
     DATABASE_URL: Joi.string().required().description('DATABASE URL'),
+    DATABASE_NAME: Joi.string().required().description('DATABASE NAME'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
@@ -22,7 +23,7 @@ const envVarsSchema = Joi.object()
     SMTP_PORT: Joi.number().description('port to connect to the email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
     SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),    
+    SMTP_PASSWORD: Joi.string().description('password for email server'),
   })
   .unknown();
 
@@ -41,6 +42,7 @@ module.exports = {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: envVars.DATABASE_NAME,
     },
   },
   jwt: {
