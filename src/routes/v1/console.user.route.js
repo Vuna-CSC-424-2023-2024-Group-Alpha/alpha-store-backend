68f6ab21cbf8dcce12f016be4e0018d753c9f910
Gideon <gideons564@gmail.com>
2023-04-25 13:21:58 +0100

    Add the console user docs

--- src/routes/v1/console.user.route.js
+++ src/routes/v1/console.user.route.js
@@ -7,8 +7,267 @@ const consoleUserController = require('../../controllers/console.user.controller
 const router = express.Router();
 
 router.get('/:consoleUserId', auth('getUsers'), consoleUserController.getConsoleUser);
-router.patch('/:consoleUserId', auth('manageUsers'), validate(consoleUserValidation.updateConsoleUser), consoleUserController.updateConsoleUser);
-router.post('/invite', auth('inviteConsoleUser'), validate(consoleUserValidation.inviteConsoleUser), consoleUserController.inviteConsoleUser);
+router.patch(
+  '/:consoleUserId',
+  auth('manageUsers'),
+  validate(consoleUserValidation.updateConsoleUser),
+  consoleUserController.updateConsoleUser
+);
+router.post(
+  '/invite',
+  auth('inviteConsoleUser'),
+  validate(consoleUserValidation.inviteConsoleUser),
+  consoleUserController.inviteConsoleUser
+);
 router.post('/accept-invite/:token', validate(consoleUserValidation.acceptInvite), consoleUserController.acceptInvite);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Console Teams
+ *   description: Console User Team for management
+ */
+
+/**
+ * @swagger
+ * /console/teams:
+ *   get:
+ *     summary: Get all console users
+ *     description: Retrieves all console users that make a team
+ *     tags: [Console Teams]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *           description: OK
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   consoleUsers:
+ *                     type: array
+ *                     items:
+ *                       $ref: '#/components/schemas/ConsoleUser'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /console/teams/{consoleUserId}:
+ *   get:
+ *     summary: Get a Console user
+ *     description: Retrieve a Console User account with the provided {consoleUserId}.
+ *     tags: [Console Teams]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: consoleUserId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ConsoleUser'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Update a Console user
+ *     description: Update a Console User account with the provided {consoleUserId}.
+ *     tags: [Console Teams]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: consoleUserId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: console user id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               lastName:
+ *                 type: string
+ *               firstName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               role:
+ *                 type: string
+ *                 enum: ['administrator','manager','operations','support','webmanager']
+ *               dateOfBirth:
+ *                 type: Date
+ *               location:
+ *                 type: string
+ *               phoneNumber:
+ *                 type: string
+ *             example:
+ *               email: fake@example.com
+ *               firstName: John
+ *               lastName: Doe
+ *               role: administrator
+ *               location: 'Ikeja, Lagos State'
+ *               dateOfBirth: '2001-11-23'
+ *               phoneNumber: '+234011111111'
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/ConsoleUser'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
+
+/**
+ * @swagger
+ * /console/teams/update-status/{consoleUserId}:
+ *   patch:
+ *     summary: Update a Console user status
+ *     description: Update status of the console user account of the provided {consoleUserId}.
+ *     tags: [Console Teams]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: consoleUserId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Console user id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *                 enum: ['active', 'inactive']
+ *             example:
+ *               status: inactive
+ *     responses:
+ *       "204":
+ *         description: No content
+ *       "400":
+ *         $ref: '#/components/responses/ValidationError'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
+
+/**
+ * @swagger
+ * /console/teams/invite:
+ *   post:
+ *     summary: Invite a new team member (Console user)
+ *     description: Invite a new team member (Console user) to the console, the user to be invited receives an email with link to complete the invite.
+ *     tags: [Console Teams]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               lastName:
+ *                 type: string
+ *               firstName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               role:
+ *                 type: string
+ *                 enum: ['administrator','manager','operations','support','webmanager']
+ *             example:
+ *               email: fake@example.com
+ *               firstName: John
+ *               lastName: Doe
+ *               role: administrator
+ *     responses:
+ *       "204":
+ *         description: No content
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ */
+
+/**
+ * @swagger
+ * /console/teams/accept-invite/{token}:
+ *   post:
+ *     summary: Accept invite of a new team member (Console user)
+ *     description: Accept invite of a new team member (Console user) to the console, this completes the invitation of a new console user.
+ *     tags: [Console Teams]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: token from invite console user email
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               password:
+ *                 type: string
+ *               confirm_password:
+ *                 type: string
+ *             example:
+ *               confirm_password: str0ngp@ssword
+ *               password: str0ngp@ssword
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ConsoleUser'
+ *       "400":
+ *         $ref: '#/components/responses/ValidationError'
+ */
