cbf5cedcd605b1562097d607ff65a263ee364cb3
Gideon <gideons564@gmail.com>
2023-05-09 10:53:16 +0100

    Add app.service

---
+++ src/services/app.service.js
@@ -0,0 +1,40 @@
+//
+const { App } = require('../routes');
+
+// app.service is a placeholder for any plateform being built
+// names can be  modified base on requirement.
+
+const createApp = async (req) => {
+  let images;
+  // add images to the app object
+  if (req.files) {
+    images = req.files.map((file) => file.path);
+  }
+
+  try {
+    const app = await App.create({
+      ...req.body,
+      images,
+    });
+
+    return app;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getApp = async () => {
+  const apps = await App.find();
+  return apps;
+};
+
+const getAppById = async (id) => {
+  const app = await App.findOne({ _id: id });
+  return app;
+};
+
+module.exports = {
+  createApp,
+  getApp,
+  getAppById,
+};
