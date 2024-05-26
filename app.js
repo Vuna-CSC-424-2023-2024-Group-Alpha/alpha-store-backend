0ed1e9d146e346a6e68868f653cd7ebc87eaa1b5
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-05-26 16:44:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import express, { json } from "express";
+import express from "express";
 import cors from "cors";
 
 import connectDB from "./src/utilities/connectDb.js";
 import authRoute from "./src/routes/auth.route.js";
+import ecommerceRoute from "./src/routes/ecommerce.route.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoute);
+app.use("/ecommerce", ecommerceRoute);
 
 await connectDB(app);
