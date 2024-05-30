2b126eb32524f627433a0c1cdc6471b31a9cf229
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-05-30 15:25:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,4 +12,10 @@ app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/ecommerce", ecommerceRoute);
 
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Alpha Store API Built by Adekolu Samuel Oluwaseun (Samixx Yasuke)</h1>`
+  );
+});
+
 await connectDB(app);
